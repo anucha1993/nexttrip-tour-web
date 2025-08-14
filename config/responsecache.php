ccbc09c195cb51b7ca5f919f722d92ddbf1b22d8
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2025-08-14 20:36:40 +0700

    update

--- config/responsecache.php
+++ config/responsecache.php
@@ -14,6 +14,7 @@ return [
      * When using the default CacheRequestFilter this setting controls the
      * default number of minutes responses must be cached.
      */
+    
     'cache_lifetime_in_minutes' => 60 * 24 * 7,
 
     /*
